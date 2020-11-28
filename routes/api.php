1c4ced3deb947165dae0d8e42ec7bf28355b6939
y-kotti <garoyann1@gmail.com>
2020-11-28 18:11:06 +0900

    TODOAPIとテストコード追加

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/todos', 'App\Http\Controllers\TodoController@index');
+Route::get('/todo/{id}', 'App\Http\Controllers\TodoController@show');
+Route::post('/todo', 'App\Http\Controllers\TodoController@store');
+Route::delete('/todo/{id}', 'App\Http\Controllers\TodoController@destroy');
+Route::patch('/todo/{todo}', 'App\Http\Controllers\TodoController@update');
